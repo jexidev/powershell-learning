6f2474997dbba2d14505cf8199a3597ab537cabd
jexidev <169895029+jexidev@users.noreply.github.com>
2025-05-19 18:54:39 +0100

    Added user confirmation for backup and press any key to exit

--- scripts/Challenge 6 - PowerShell Automated Backup.ps1
+++ scripts/Challenge 6 - PowerShell Automated Backup.ps1
@@ -5,6 +5,7 @@ Write a script that:
 3. Creates a timestamped backup copy of the source folder. 
 4. Preserves subfolders and files inside.
 
+# Addition: Confirm with user before backup and press any key to exit
 #>
 
 # Step 1 - Ask the user for source folder
@@ -25,16 +26,28 @@ do { $backupFolder = Read-Host "Enter the backup destination Folder"
 
 # Step 3 - Create Backup Folder
 
+# Ask User for Confirmation. 
+$userConfirmation = Read-Host "Please confirm you would like to backup '$sourceFolder' to '$backupFolder' (Yes/No)"
+
+# Set name for backup folder.
 $timestamp = Get-Date -Format "ddMMyyyy_HHmm"
 $backupPath = "$backupFolder\Backup_$timestamp"
 
-# Test if backup exists
+if ($userConfirmation.ToLower() -eq "yes") {
+    # Test if backup exists
+   if (!(Test-Path -Path $backupPath)) {
+    New-Item -ItemType Directory -Path $backupPath | Out-Null
 
-if (!(Test-Path -Path $backupPath)) {
-    New-Item -ItemType Directory -Path $backupPath
+   }
+
+    # Copy items to backup 
+    Copy-Item -Path $sourceFolder -Destination $backupPath -Recurse -Force
+    Write-Output "Backup completed! Files copied to: $backupPath"
+
 }
 
-#Copy items to backup 
-
-Copy-Item -Path $sourceFolder -Destination $backupPath -Recurse
-Write-Output "Backup completed! Files copied to: $backupPath"
+# Press any key to exit
+else { Write-Host "Backup Cancelled: Press any key to exit"
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKey")    
+    
+}
